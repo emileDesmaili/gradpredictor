5ea11b24c27089bffc371091115df82e27dac2c2
EmE <emile.esmaili@ekimetrics.com>
2022-07-01 18:10:29 +0200

    first model results

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
 import pickle
+import numpy as np
 
 # PAGE SETUP
 st.set_page_config(
@@ -40,7 +41,6 @@ with page_container:
 @st.cache()
 def load_csv():
     return pd.read_csv('data/admission_data.csv')
-@st.cache():
 
 @st.cache(allow_output_mutation=True)
 def load_predictor():
@@ -52,4 +52,39 @@ data = load_csv()
 
 if page =="ChanceMe!":
     model = load_predictor()
-    
+
+    with st.form('Chancer'):
+        filter1, filter2, filter3, filter4, filter5, filter6 = st.columns(6)
+        with filter1:
+            sop = st.slider('SOP strength',1,5,step=1)
+        with filter2:
+            research = st.checkbox('Research Experience?')
+        with filter3:
+            lor = st.slider('LORs strength',1,5,step=1)
+        with filter4:
+            toefl = st.number_input('TOEFL score',max_value=120,step=1)
+        with filter5:
+            gre = st.number_input('GRE score',max_value=340,step=10)
+        with filter6:
+            gpa = st.number_input('GPA out of 10',max_value=10.,step=0.01)
+        
+        
+        submitted = st.form_submit_button('ChanceMe!')
+    proba_t5 = model.predict(np.array([gre, toefl, 1, sop, lor, gpa, research]).reshape(1,-1))
+    proba_t4 = model.predict(np.array([gre, toefl, 2, sop, lor, gpa, research]).reshape(1,-1))
+    proba_t3 = model.predict(np.array([gre, toefl, 3, sop, lor, gpa, research]).reshape(1,-1))
+    proba_t2 = model.predict(np.array([gre, toefl, 4, sop, lor, gpa, research]).reshape(1,-1))
+    proba_t1 = model.predict(np.array([gre, toefl, 5, sop, lor, gpa, research]).reshape(1,-1))
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.metric('Probability of getting in a Tier 1 University', np.round(proba_t1,3))
+    with col2:
+        st.metric('Probability of getting in a Tier 2 University', np.round(proba_t2,3))
+    with col3:
+        st.metric('Probability of getting in a Tier 3 University', np.round(proba_t3,3))
+    with col4:
+        st.metric('Probability of getting in a Tier 4 University', np.round(proba_t4,3))
+    with col5:
+        st.metric('Probability of getting in a Tier 5 University', np.round(proba_t5,3))
+
+
